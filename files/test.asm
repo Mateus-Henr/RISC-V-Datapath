3e33534ce8da57b2f64d084a6ce54814a1ccd8ea
Mateus-Henr <mateush.study@gmail.com>
2022-07-01 11:26:09 -0300

    Add sketch files

---
+++ files/test.asm
@@ -0,0 +1,17 @@
+# Colocando valor base na memória
+# addi x2, x0, 7
+# sw x2, 4(x0)
+
+lw x1, 4(x0)
+add x2, x1, x0
+
+add x1, x1, x2
+add x1, x1, x2
+sub x1, x1, x2
+sub x1, x1, x2
+
+beq x1, x2, SAIDA
+
+# Caso o fluxo venha para cá, seu processador está errado
+add x1, x1, x1
+sw x1, 0(x0)
